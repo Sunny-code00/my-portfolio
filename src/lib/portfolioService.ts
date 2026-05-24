2c8d0fc8c0e7f06da63226b032ab74cd9cc1a2e7
Sunny kumar <kumarsunny13012005@gmail.com>
2026-05-24 15:53:13 +0530

    Initial portfolio upload

---
+++ src/lib/portfolioService.ts
@@ -0,0 +1,11 @@
+export const fetchProjects = async () => {
+  return []
+}
+
+export const fetchCertificates = async () => {
+  return []
+}
+
+export const fetchTechStacks = async () => {
+  return []
+}
